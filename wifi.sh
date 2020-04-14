fa00dbcb77489ac31644865bf83ac6e78cae2671
cchack1999 <63628425+cchack1999@users.noreply.github.com>
2020-04-14 11:12:36 +0700

    Add files via upload

---
+++ wifi.sh
@@ -0,0 +1,78 @@
+#!/bin/bash    
+clear
+echo "            ,             /@@@@@=-"
+sleep 0.03
+echo "            \\\            @@@@@@@@@@=-"
+sleep 0.03
+echo "             \\\          _\@/\@@@@@=-"
+sleep 0.03
+echo "              \\\        /_ +\ \@@@@@=-"
+sleep 0.03
+echo "        ,      \\\      (_/   )  \@@@@=-"
+sleep 0.03
+echo "        \\\      \\\     (_____)    \@@=-"
+sleep 0.03
+echo "        _\\\_/\\_ _\\\__  /     \     ~~"
+sleep 0.03
+echo "  ____,/+-  '/\\\  { \_|___(__ ) "
+sleep 0.03
+echo " >             \\\  )_|/  ___  \\\ "
+sleep 0.03
+echo " \_//--\\___/    \\\.' / <-q-p-> \\\ "
+sleep 0.03
+echo "    _//   )      \\\(\\/\\ <-d-b-> /___"
+sleep 0.03
+echo "   _____  /         \\\/ \  \|/  //   \__"
+sleep 0.03
+echo "  /     \/          /   \_____//     \_\ "
+sleep 0.03
+echo " | /\_  |         (_  /______\\     |||"
+sleep 0.03
+echo " | \_ | |         | \|   <    \\    /||"
+sleep 0.03
+echo " \_\_\ \/     ____\  |____\    \)  / ||"
+sleep 0.03
+echo "       /    _/  <____)\    (      / //\\"
+sleep 0.03
+echo "      /   _/           \    \    (  \\//"
+sleep 0.03
+echo "     (   /              )  / \    \  \/"
+sleep 0.03
+echo "     /  /              /  /   \    )"
+sleep 0.03
+echo " ---/  /   Wifi ---/  /-----)  /-----"
+sleep 0.03
+echo "  _/__/   Hack!  _/__/     /  /"
+sleep 0.03
+echo " /__/              /__/     _/__/"
+sleep 0.03
+echo "                           /__/"
+echo " ======================================="
+echo " ==         CharlieChaplinHack           =="
+echo " ======================================="
+sleep 3
+echo ""
+echo "Jangan lupa di SUBSCRIBE bosqu Channel saya"
+sleep 3
+clear
+echo "Masukan nama wifi terget"
+read nama
+sleep 3
+echo ""
+echo "Masukan alamat dhcp wifi target"
+read dhcp
+sleep 3
+echo ""
+echo "Silahkan tunggu proses crack nya"
+sleep 3
+echo ""
+echo "Mungkin akan sedikit memakan waktu"
+sleep 60
+echo ""
+echo "Crack wifi $nama berhasil !"
+sleep 3
+echo ""
+echo "Silahkan cek file password $nama di internal storage"
+sleep 3
+echo ""
+echo "Jangan lupa SUBSCRIBE, LIKE & SHARE ya bosqu"
